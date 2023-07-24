2ca126fcf50362f9a014d7c893f474c92334cb6b
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-07-24 13:23:46 +0800

    Add sleep clock

--- js/sleep_time.js
+++ js/sleep_time.js
@@ -1,6 +1,7 @@
 let sleepTimer; // Переменная для хранения ID таймера
 const mainDiv = document.getElementById("main");
 const bgVideo = document.getElementById("bgvideo");
+const clockDiv = document.getElementById("sleep_clock");
 
 function startSleepTimer() {
     // Сбрасываем предыдущий таймер (если он был)
@@ -15,7 +16,9 @@ function sleep() {
     // Ваша функция, которую нужно вызвать после бездействия мыши в течение 1 минуты
     console.log('Функция запущена после бездействия мыши в течение 1 минуты');
     mainDiv.classList.add('hidden');
+    bgVideo.classList.add('darkBg');
     bgVideo.classList.add('slideUp');
+    clockDiv.classList.remove("hidden"); // Show the clock
 }
 
 function unSleep() {
@@ -23,6 +26,72 @@ function unSleep() {
     mainDiv.style = '';
     mainDiv.classList.remove('hidden');
     bgVideo.classList.remove('slideUp');
+    bgVideo.classList.remove('darkBg');
+
+    clockDiv.classList.add("hidden"); // Hide the clock again
 }
+
+var hoursContainer = document.querySelector('.hours')
+var minutesContainer = document.querySelector('.minutes')
+var secondsContainer = document.querySelector('.seconds')
+var tickElements = Array.from(document.querySelectorAll('.tick'))
+var last = new Date(0)
+last.setUTCHours(-1)
+var tickState = true
+
+function updateClockSleep() {
+    var now = new Date
+    var lastHours = last.getHours().toString()
+    var nowHours = now.getHours().toString()
+    if (lastHours !== nowHours) {
+        updateContainer(hoursContainer, nowHours)
+    }
+    var lastMinutes = last.getMinutes().toString()
+    var nowMinutes = now.getMinutes().toString()
+    if (lastMinutes !== nowMinutes) {
+        updateContainer(minutesContainer, nowMinutes)
+    }
+    var lastSeconds = last.getSeconds().toString()
+    var nowSeconds = now.getSeconds().toString()
+    if (lastSeconds !== nowSeconds) {
+        //tick()
+        updateContainer(secondsContainer, nowSeconds)
+    }
+    last = now
+}
+
+function tick() {
+    tickElements.forEach(t => t.classList.toggle('tick-hidden'))
+}
+
+function updateContainer(container, newTime) {
+    var time = newTime.split('')
+    if (time.length === 1) {
+        time.unshift('0')
+    }
+    var first = container.firstElementChild
+    if (first.lastElementChild.textContent !== time[0]) {
+        updateNumber(first, time[0])
+    }
+    var last = container.lastElementChild
+    if (last.lastElementChild.textContent !== time[1]) {
+        updateNumber(last, time[1])
+    }
+}
+
+function updateNumber(element, number) {
+    //element.lastElementChild.textContent = number
+    var second = element.lastElementChild.cloneNode(true)
+    second.textContent = number
+    element.appendChild(second)
+    element.classList.add('move')
+    setTimeout(function () {
+        element.classList.remove('move')
+    }, 990)
+    setTimeout(function () {
+        element.removeChild(element.firstElementChild)
+    }, 990)
+}
+setInterval(updateClockSleep, 100)
 
 document.addEventListener('mousemove', startSleepTimer);
