d86968d8dbceabbf444ec12e1d9d142bcb344d30
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-09-17 20:03:34 +0800

    Actions for whole page context menu

--- js/settings_menu.js
+++ js/settings_menu.js
@@ -1,10 +1,26 @@
+const addIcon = `<span class="material-symbols-outlined">add</span>`;
+const wallpaperIcon = `<span class="material-symbols-outlined">wallpaper</span>`;
+const editorModeIcon = `<span class="material-symbols-outlined">app_registration</span>`;
+
 function showSettingsMenu(e) {
     //TODO: Add actions for main context menu
-    const menuItems = [
-        { 
-            content: `Add panel`,
-            events:
-            { click: e => addNewPanel() } 
+    const menuItems = [{
+            content: `${addIcon}Add panel`,
+            events: {
+                click: e => createPanel()
+            }
+        },
+        {
+            content: `${editorModeIcon}Editor mode`,
+            events: {
+                click: e => enableEditor()
+            }
+        },
+        {
+            content: `${wallpaperIcon}Change Wallpaper`,
+            events: {
+                click: e => changeWallpaper()
+            }
         },
     ];
     ContextMenuHelper.getInstance().showMenu(e, menuItems);
@@ -37,8 +53,47 @@ function enableEditor() {
     }
 }
 
-function uploadFile() {
+
+function changeWallpaper() {
+    const popup = document.getElementById("wallpaper-change-popup");
+    popup.style.display = "block";
+
+    const closePopupButton = document.getElementById("closeWallpaperChangePopup");
+    closePopupButton.addEventListener("click", function () {
+        popup.style.display = "none";
+    });
+
+    const wallpaperUrlInput = document.getElementById("wallpaperUrlInput");
+    const bgvideo = document.getElementById("bgvideo");
+
+    document.getElementById("changeWallpaper").addEventListener("click", function () {
+        const newWallpaperUrl = wallpaperUrlInput.value;
+        if (newWallpaperUrl) {
+            bgvideo.src = newWallpaperUrl;
+            // Сохраните новый URL в куки
+            document.cookie = `bgvideo=${newWallpaperUrl}`;
+            popup.style.display = "none";
+        }
+    });
+
+    const videoCookie = document.cookie.split(';').find(cookie => cookie.trim().startsWith('bgvideo='));
+    if (videoCookie) {
+        const videoUrl = videoCookie.split('=')[1].trim();
+        bgvideo.src = videoUrl;
+    }
 }
+
+function setWallpaper() {
+    const bgvideo = document.getElementById("bgvideo");
+
+    const videoCookie = document.cookie.split(';').find(cookie => cookie.trim().startsWith('bgvideo='));
+    if (videoCookie) {
+        const videoUrl = videoCookie.split('=')[1].trim();
+        bgvideo.src = videoUrl;
+    }
+}
+
+function uploadFile() {}
 
 function downloadFile() {
     const jsContent = "var panel_schema = " + localStorage.getItem('panel_config');
